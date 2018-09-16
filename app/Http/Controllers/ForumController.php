250853c6c82a3b82a7c04eb5ed6babc2aa35ad66
colgay <vip-colgate@hotmail.com>
2018-09-17 01:37:32 +0800

    create forum

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class ForumController extends Controller {
 
-    public function create()
-    {
+    public function index() {
+        $forums = Forum::all();
+        return view('forum.index', ['forums' => $forums]);
+    }
+
+    public function create() {
         return view('forum.create');
     }
 }
--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -5,37 +5,26 @@
     <div class="container h-100">
         <div class="row h-100">
             <div class="col-12 my-auto">
-                {!! Form::open(['route' => 'forum.store', 'id' => 'createForumForm', 'class' => 'card mx-auto']) !!}
-                <div class="card-body">
-                    <h2 class="card-title">建立討論區</h2>
-                    
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-
-                    <div class="form-group">
-                        {!! Form::label('name', '名稱') !!}
-                        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                <div class="card mx-auto" id="createForum">
+                    <div class="card-body">
+                        <h2 class="card-title">新起一個討論區</h2>
+                        <form method="post" action="/forum" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="name">討論區個名</label>
+                                <input name="name" type="text" class="form-control" id="nameid" placeholder="討論區叫咩名">
+                            </div>
+                            <div class="form-group">
+                                <label for="desc">討論區介紹</label>
+                                <input name="desc" type="text" class="form-control" id="descid" placeholder="介紹下個討論區係點">
+                            </div>
+                            <div class="form-group">
+                                <label for="url">討論區網址</label>
+                                <input name="url" type="text" class="form-control" id="urlid" placeholder="討論區條link係點">
+                            </div>
+                            <button type="submit" class="btn btn-primary">整</button>
+                        </form>
                     </div>
-
-                    <div class="form-group">
-                        {!! Form::label('desc', '資訊') !!}
-                        {!! Form::text('desc', null, ['class' => 'form-control']) !!}
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('url', '網址') !!}
-                        {!! Form::text('url', null, ['class' => 'form-control']) !!}
-                    </div>
-
-                    {!! Form::submit('建立', ['class' => 'btn btn-primary']) !!}
-                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
